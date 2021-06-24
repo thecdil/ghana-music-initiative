a159697e1fcebd1863e75ed7a0c5771445aa3b98
owikle <owikle@users.noreply.github.com>
2021-06-24 22:39:37 +0000

    deploy: 0f71d88d514095559b225e8bb1ea541ca15296dc

---
+++ assets/js/lunr-store.js
@@ -0,0 +1,121 @@
+var store = [{  
+"title": "SeneGal","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Senegal","subject": "dance; Ga; Senegal","location": "Accra", 
+"id": "uighana-006a"
+
+},{  
+"title": "Sobima","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Sobima","subject": "dance; Ga; Sobima","location": "Accra", 
+"id": "uighana-006b"
+
+},{  
+"title": "Sanka","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Sanka","subject": "dance; Ga; Sanka","location": "Accra", 
+"id": "uighana-006c"
+
+},{  
+"title": "Gahu","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Gahu","subject": "dance; Eʋe; Gahu","location": "Accra", 
+"id": "uighana-006d"
+
+},{  
+"title": "Kpanlogo","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Kpanlogo","subject": "dance; Ga; Kpanlogo","location": "Accra", 
+"id": "uighana-006e"
+
+},{  
+"title": "Kpatsa","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Kpatsa","subject": "dance; Ga; Eʋe; Kpatsa","location": "Accra", 
+"id": "uighana-006f"
+
+},{  
+"title": "Femefeme","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Femefeme","subject": "dance; Ga; Femefeme","location": "Accra", 
+"id": "uighana-006g"
+
+},{  
+"title": "Setse","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Setse","subject": "dance; Ga; Setse","location": "Accra", 
+"id": "uighana-006h"
+
+},{  
+"title": "Gome","date": "2005","creator": "Felix Akusa Adjetey Sowah","description": "The Teshie-Nungua Cultural Dance Troupe performs Gome","subject": "dance; Ga; Gome","location": "Accra", 
+"id": "uighana-006i"
+
+},{  
+"title": "Kpanlogo Drum","date": "2007-04-02","creator": "Allan Fraser – Strobilus","description": "Musician playing two kpanlogo drums with sticks","subject": "dance; Kpanlogo; Ga","location": "Accra", 
+"id": "uighana-007"
+
+},{  
+"title": "Ehtnicities of Ghana","date": "1966","creator": "Accra : Survey Division","description": "Map showing the predmoninant boundaries of various ethnicities in Ghana","subject": "Ga; Akan; Dagbamba; Eʋe","location": "Accra", 
+"id": "uighana-008"
+
+},{  
+"title": "Kpeve ceremonial Dancer","date": "2018","creator": "Barry Bilderback","description": "A female dancer in the Kpeve village is seen displaying the physical embodiment of the transpossetion practice evidenced by chalk on the face, the staw skirt, and the ceremonial knife she wields. The knife is used to thrust into oneself in the straw skirt, often leaving scratches and abrasions.","subject": "Eʋe; Volta; ceremonial dance;","location": "Kpeve", 
+"id": "uighana-009"
+
+},{  
+"title": "Blessing of the Young Prophets","date": "2018","creator": "Barry Bilderback","description": "The young prophets of the Kpeve village are blessed by the outer shrine in front of a physical depiction of Mawu.","subject": "Eʋe; Volta; ceremonial dance;","location": "Kpeve", 
+"id": "uighana-010"
+
+},{  
+"title": "Kpando ceremonial Dancer","date": "2018","creator": "Barry Bilderback","description": "A dancer in the Kpando village prepares for a ceremonial dance evidenced by the straw skirt.","subject": "Eʋe; Volta; ceremonial dance;","location": "Kpando", 
+"id": "uighana-011"
+
+},{  
+"title": "Tamale Circular Dance","date": "2018","creator": "Barry Bilderback","description": "Dancers in Tamale Gather in a circle for a community performance.","subject": "Dagbamba; northern;","location": "Tamale", 
+"id": "uighana-012"
+
+},{  
+"title": "Kusun Cultral Center: Ga Perform Eʋe","date": "2018","creator": "Barry Bilderback","description": "Dancers at the Kusun Cultural Center in Nungua perform a variation of a traditional Eʋe dance with Ga ensemble accompaniment. Hunor Gatukpe Dogah directs the ensemble.","subject": "Accra; Ga; Eʋe; Nungua","location": "Nungua, Accra", 
+"id": "uighana-013"
+
+},{  
+"title": "Kusun Cultral Center: Ga Perform Eʋe","date": "2018","creator": "Barry Bilderback","description": "Dancers at the Kusun Cultural Center in Nungua perform a variation of a traditional Eʋe dance with Ga ensemble accompaniment. Hunor Gatukpe Dogah directs the ensemble.","subject": "Accra; Ga; Eʋe; Nungua","location": "Nungua, Accra", 
+"id": "uighana-014"
+
+},{  
+"title": "Kusun Cultral Center: Ga Perform Eʋe","date": "2018","creator": "Barry Bilderback","description": "Dancers at the Kusun Cultural Center in Nungua perform a variation of a traditional Eʋe dance with Ga ensemble accompaniment. Hunor Gatukpe Dogah directs the ensemble.","subject": "Accra; Ga; Eʋe; Nungua","location": "Nungua, Accra", 
+"id": "uighana-015"
+
+},{  
+"title": "Kusun Cultral Center: Ga Perform Eʋe","date": "2018","creator": "Barry Bilderback","description": "Dancers at the Kusun Cultural Center in Nungua perform a variation of a traditional Eʋe dance with Ga ensemble accompaniment. Hunor Gatukpe Dogah directs the ensemble.","subject": "Accra; Ga; Eʋe; Nungua","location": "Nungua, Accra", 
+"id": "uighana-016"
+
+},{  
+"title": "Homowo Festival","date": "2018","creator": "Barry Bilderback","description": "Elders dressed in white process through the road as part of the Homowo festival.","subject": "Homowo; Ga; harvest;","location": "Prampram", 
+"id": "uighana-017"
+
+},{  
+"title": "Ga Instruments","date": "2018","creator": "Barry Bilderback","description": "Instrument storage facility in the Ga region.","subject": "Ga; instruments", 
+"id": "uighana-018"
+
+},{  
+"title": "Tamale Houses","date": "2018","creator": "Barry Bilderback","description": "Houses in Tamale indicative by the clay walls and foundation with the straw thatched roofs.","subject": "Dagbamba; Tamale; houses","location": "Tamale", 
+"id": "uighana-019"
+
+},{  
+"title": "Tamale Drum Ensemble","date": "2018","creator": "Barry Bilderback","description": "Four barrell shaped drums of varying sizes are used to accompany a recreational ring dance.","subject": "Dagbamba; Tamale; ensemble","location": "Tamale", 
+"id": "uighana-020"
+
+},{  
+"title": "Tamale Recreational Dance","date": "2018","creator": "Barry Bilderback","description": "A recreational ring dance comprised of side-stepping. Over time, the participant dancers move counter clock wise. Some dancers can be seen wearing ankle bells.","subject": "Dagbamba; Tamale; ensemble; recreation; dance","location": "Tamale", 
+"id": "uighana-021"
+
+},{  
+"title": "Hunor Gatukpe DoGah and Eʋe Drum Ensemble in the Kpeve Village","date": "2018","creator": "Barry Bilderback","description": "A drum ensemble in the Volta region performs for a ceremonial dance featuring the young prophets. Hunor Gatukpe DoGah is seen playing the atopani.","subject": "Kpeve; Eʋe; ensemble; ceremony; atopani","location": "Kpeve", 
+"id": "uighana-022"
+
+},{  
+"title": "Nunya Music Academy Brass Band","date": "2018","creator": "Barry Bilderback","description": "The Nunya Music Academy Brass Band performs under the direction of Prosper Gbolonyo.","subject": "brass; Dzodze","location": "Dzodze", 
+"id": "uighana-023"
+
+},{  
+"title": "Nunya Music Academy Brass Band","date": "2018","creator": "Barry Bilderback","description": "The Nunya Music Academy Brass Band performs under the direction of Prosper Gbolonyo.","subject": "brass; Dzodze","location": "Dzodze", 
+"id": "uighana-024"
+
+},{  
+"title": "Nunya Music Academy Brass Band","date": "2018","creator": "Barry Bilderback","description": "The Nunya Music Academy Brass Band performs under the direction of Prosper Gbolonyo.","subject": "brass; Dzodze","location": "Dzodze", 
+"id": "uighana-025"
+
+},{  
+"title": "Homowo Procession","date": "2018","creator": "Barry Bilderback","description": "Elders dressed in white process through the road as part of the Homowo festival. One can be seen blowing into a horn [Lãdzo?]","subject": "Homowo; Ga; harvest;","location": "Prampram", 
+"id": "uighana-026"
+
+},{  
+"title": "Ga Ensemble at Homowo","date": "2018","creator": "Barry Bilderback","description": "Ga musicians seen performing on custom drums, resembling Eʋe atopani at Homowo.","subject": "Homowo; Ga; harvest; ensemble","location": "Prampram", 
+"id": "uighana-027"
+
+}];
